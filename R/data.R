843e6f1dcdb19368159c337430efdc2f8e90e18f
ZJJZ1124 <72218003+ZJJZ1124@users.noreply.github.com>
2020-11-01 16:29:12 +0800

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' Dataset containing information from the Fatality Analysis Reporting System for 2013
+#'
+#' Dataset containing information from the Fatality Analysis Reporting System for 2013
+#'
+#'
+#' @format A data frame with 30202 rows and 50 variables
+#' @source \url{https://cdan.nhtsa.gov/}
+"fars_2013"
+
+#' Dataset containing information from the Fatality Analysis Reporting System for 2014
+#'
+#' Dataset containing information from the Fatality Analysis Reporting System for 2014
+#'
+#'
+#' @format A data frame with 30056 rows and 50 variables
+#' @source \url{https://cdan.nhtsa.gov/}
+"fars_2014"
+
+#' Dataset containing information from the Fatality Analysis Reporting System for 2015
+#'
+#' Dataset containing information from the Fatality Analysis Reporting System for 2015
+#'
+#'
+#' @format A data frame with 32166 rows and 50 variables
+#' @source \url{https://cdan.nhtsa.gov/}
+"fars_2015"
